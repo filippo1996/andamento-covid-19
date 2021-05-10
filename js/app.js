51dd043923c3ca5da5920cfb5155c745734b9d17
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-05-10 20:10:14 +0200

    add cross-check

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-var urlApi = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json';
+var urlApi = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json';
 var items = [];
-var elements = ['data','dimessi_guariti','totale_positivi','ricoverati_con_sintomi'];
+//const elements = ['data','stato','dimessi_guariti','totale_positivi','ricoverati_con_sintomi','deceduti','tamponi','nuovi_positivi','totale_positivi_test_molecolare','isolamento_domiciliare'];
+const elements = ['dimessi_guariti','totale_positivi','ricoverati_con_sintomi','deceduti','tamponi','nuovi_positivi','totale_positivi_test_molecolare','isolamento_domiciliare'];
+const daysAgo = 1;
 
 //Invochiamo la classe Ajax
 var xhr = new XMLHttpRequest();
@@ -16,15 +18,48 @@ xhr.onreadystatechange = function(){
         var resp = xhr.responseText;
         var obj = JSON.parse(resp);
         items = obj;
-        documentEle(elements,items);
-    } else{
-        console.log('errore nella chiamata');
+        timeLampse(daysAgo, items);
     }
 }
 
-function documentEle(elements,items){
+/**
+ * @param {Number} daysAgo - The number
+ * @param {Array} items - The array
+ */
+function timeLampse(daysAgo, items){
+    let present,last;
+
+    //Lunghezza array
+    let lengthArr = items.length - 1;
+    //Recupero indice array temporale
+    let arrayTime = lengthArr - daysAgo;
+    
+    present = items[lengthArr];
+    last = items[arrayTime];
+    //invoke function
+    documentEle(elements, present, last);
+}
+
+function documentEle(elements, present, last){
+    let message = '';
+    let calc;
     for(const element of elements){
-        var ele = document.querySelector('#' + element);
-        ele.innerHTML = items[0][element];
+        let ele = document.querySelector('#' + element);
+        let eleLast = document.querySelector('#' + element + '_last');
+        if(element === 'stato' && present[element] === 'ITA'){
+            ele.src = 'img/ico/italy.ico';
+        }
+        ele.innerHTML = present[element];
+
+        if(present[element] > last[element]){
+            calc = present[element] - last[element];
+            eleLast.classList.add('txt-red');
+            message = '+ ' + calc + ' in più rispetto a ' + daysAgo + ' giorno fa';
+        }else if(present[element] < last[element]){
+            calc = last[element] - present[element];
+            eleLast.classList.add('txt-green');
+            message = '- ' + calc + ' in meno rispetto a ' + daysAgo + ' giorno fa';
+        }
+        eleLast.innerHTML = message;
     }
 }
